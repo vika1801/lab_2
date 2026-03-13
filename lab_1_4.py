fa8f0e06613559bc596b4299e45adaef1287f16e
Вика Сопова <vksspv@gmail.com>
2026-03-13 20:44:09 +0700

    lab_1_4 Сумма и количество чисел последовательности. Исправление

--- lab_1_4.py
+++ lab_1_4.py
@@ -1,17 +1,24 @@
 # lab_1_4.py
 # Сумма и количество чисел последовательности
 
-total = 0
-count = 0
+total_sum = 0
+numbers_count = 0
 
 print("Введите целые числа. Для завершения введите 0.")
 
-num = int(input())
+while True:
 
-while num != 0:
-    total = total + num
-    count = count + 1
-    num = int(input())
+    try:
+        number = int(input())
 
-print("Сумма:", total)
-print("Количество:", count)
+        if number == 0:
+            break
+
+        total_sum += number
+        numbers_count += 1
+
+    except ValueError:
+        print("Ошибка: необходимо вводить целое число.")
+
+print("Сумма:", total_sum)
+print("Количество:", numbers_count)
